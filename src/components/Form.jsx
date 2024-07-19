0b389ca099f2f44c88c3b2e10da3a035fb4b825e
Mahsa <arabzadehmah@gmail.com>
2024-07-20 00:55:10 +0430

    Feat(form): feat handleSubmit func and prevent to reload page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,38 @@
 function Form() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div
-      className="px-8 py-2 flex justify-center"
-      style={{ background: "#549198", color: "#fff" }}
+    <form
+      className="px-8 py-2 flex justify-center gap-2 items-center"
+      style={{ background: "#549198" }}
+      onSubmit={handleSubmit}
     >
-      <h3 className="text-xl">What do you need for your trip? 👜 </h3>
-    </div>
+      <h3 className="text-xl flex justify-center" style={{ color: "#fff" }}>
+        What do you need for your trip? 👜{" "}
+      </h3>
+
+      <select className="rounded" style={{ outline: "none" }}>
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        className="p-2 rounded"
+        style={{ outline: "none" }}
+        type="text"
+        placeholder="Item..."
+      />
+      <button
+        className="p-2 rounded font-semibold"
+        style={{ background: "#fafbfa" }}
+      >
+        Add
+      </button>
+    </form>
   );
 }
 
